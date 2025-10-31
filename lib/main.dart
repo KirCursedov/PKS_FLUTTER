8da16a2bbaeeb25dc9e844c2c33f9c781ff63e0e
KirCursedov <yaroslav.naumov1000-7@yandex.ru>
2025-10-31 10:20:01 +0300

    Nravitsa

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       title: 'Shop',
       theme: ThemeData(
         primaryColor: const Color(0xFF1E88E5),
-        scaffoldBackgroundColor: Colors.black,
+        scaffoldBackgroundColor: Colors.white,
         fontFamily: 'Arial',
       ),
       home: const HomeScreen(),
